ac18f7c25cf56788bc55358b851f8c28abb1aa82
Arturo Gamez <arturogamez51@gmail.com>
2022-10-08 12:42:29 -0700

    adding the input button and its styles

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -32,7 +32,7 @@ const DUMMY_MEALS = [
 
     const AvailableMeals = () => {
         const mealsList = DUMMY_MEALS.map( (meal) => <MealItem key={meal.id} meal={meal} />);
-        
+
         return <section className={classes.meals}>
           <Card>
             <ul>
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import classes from './MealItem.module.css';
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
 
@@ -13,7 +14,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-
+                <MealItemForm id={props.meal.id}/>
             </div>
         </li>
     );
